22b8ed0681ac5ebd281a63ad1a15d0494d2f24d7
yonko-mo <alsantel.ma@gmail.com>
2026-03-13 02:29:37 +0200

    finished exercise_1 in session_1_homework

--- session_1_homework/exercise_1/lib/main.dart
+++ session_1_homework/exercise_1/lib/main.dart
@@ -1,121 +1,173 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Exercise1());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Exercise1 extends StatelessWidget {
+  const Exercise1({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              /// Top Banner
+              Container(
+                height: 160,
+                width: double.infinity,
+                color: const Color(0xffe4f2fd),
+              ),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+              const SizedBox(height: 16),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+              /// Small box + long box
+              Row(
+                children: [
+                  Container(
+                    width: 30,
+                    height: 20,
+                    color: const Color(0xffe0e0e0),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Container(
+                      height: 20,
+                      color: const Color(0xffe0e0e0),
+                    ),
+                  ),
+                ],
+              ),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+              const SizedBox(height: 20),
 
-  final String title;
+              const Divider(),
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+              const SizedBox(height: 20),
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+              /// Green + Orange Section
+              Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      children: [
+                        Container(height: 50, color: const Color(0xffa5d6a7)),
+                        const SizedBox(height: 8),
+                        Container(height: 50, color: const Color(0xffa5d6a7)),
+                      ],
+                    ),
+                  ),
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+                  const SizedBox(width: 16),
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+                  Expanded(
+                    flex: 3,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            height: 110,
+                            color: const Color(0xffffcc80),
+                          ),
+                        ),
+                        Container(
+                          width: 10,
+                          height: 110,
+                          color: const Color(0xfffff2e0),
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 110,
+                            color: const Color(0xffffcc80),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              /// Purple Section
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: const Color(0xffe1bee8),
+                    ),
+                  ),
+
+                  const SizedBox(width: 15),
+
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Container(height: 40, color: const Color(0xffcf93d9)),
+                        Container(height: 16, color: const Color(0xffe1bee8)),
+                        Container(height: 40, color: const Color(0xffcf93d9)),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(width: 15),
+
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: const Color(0xffe1bee8),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      color: const Color(0xfff3e5f6),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              /// Blue Section
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 60,
+                      color: const Color(0xffb2dfdc),
+                    ),
+                  ),
+
+                  const SizedBox(width: 16),
+
+                  Expanded(
+                    child: Container(
+                      height: 60,
+                      color: const Color(0xff80cbc4),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              /// Bottom bar
+              Container(
+                height: 60,
+                width: double.infinity,
+                color: const Color(0xffe0e0e0),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
